cb091e50696f3e128e960c8e41fc08e557aa872e
Sophia Mahin <sophmahin@gmail.com>
2016-04-03 17:46:56 -0400

    added expectiminimax agent class

---
+++ src/ExpectiminimaxAgent.java
@@ -0,0 +1,16 @@
+
+public class ExpectiminimaxAgent extends Agent {
+
+	@Override
+	public int movePiece(int Player) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String movePiece(int Player, int position, int dieRoll) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
